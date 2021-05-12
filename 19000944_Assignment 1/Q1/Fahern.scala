bcf446cfac09b089807d613ac3a489bdcdb5fb63
oshadha123 <62198230+oshadha123@users.noreply.github.com>
2021-05-12 12:21:36 +0530

    Add files via upload

---
+++ 19000944_Assignment 1/Q1/Fahern.scala
@@ -0,0 +1,6 @@
+object Fahern extends App{
+ def convert(x:Double)=x*1.8+32;
+
+ println(convert(35));
+
+}
---
+++ 19000944_Assignment 1/Q2/Volume.scala
@@ -0,0 +1,6 @@
+object Volume extends App{
+ def calculateVolume(r:Double)=(4*22*r*r*r)/(3*7);
+ 
+ println(calculateVolume(5));
+
+}
